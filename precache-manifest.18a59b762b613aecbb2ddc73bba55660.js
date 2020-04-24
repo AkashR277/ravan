06e563da1d8cfba922e76d99d996c2d4fd04ed6d
Akash786 <akashrathore756@gmail.com>
2020-04-24 16:15:11 +0530

    Updates

---
+++ precache-manifest.18a59b762b613aecbb2ddc73bba55660.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a25fe7b988cc2ddd03b56dbe213929",
+    "url": "/ravan/index.html"
+  },
+  {
+    "revision": "13bbddc2b0535d4eca39",
+    "url": "/ravan/static/css/main.d93a2dd9.chunk.css"
+  },
+  {
+    "revision": "642ef0b4313403c178c4",
+    "url": "/ravan/static/js/2.8712671e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ravan/static/js/2.8712671e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13bbddc2b0535d4eca39",
+    "url": "/ravan/static/js/main.d6f567c6.chunk.js"
+  },
+  {
+    "revision": "d15b9410c46da629b4f8",
+    "url": "/ravan/static/js/runtime-main.13fe9dba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ravan/static/media/logo.5d5d9eef.svg"
+  }
+]);
